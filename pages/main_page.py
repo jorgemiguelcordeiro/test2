385a7b6c18c1c00152b78e7423e5b7a19e4822bb
Jorge Miguel Cordeiro <156421211+jorgemiguelcordeiro@users.noreply.github.com>
2024-12-02 17:36:53 +0000

    Update main_page.py

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,7 +1,6 @@
 
 import streamlit as st
 
-def run():
-    st.title("Welcome to the Diabetes Prediction App")
-    st.markdown("This is the main landing page.")
-    st.markdown("Use the sidebar to navigate to other pages.")
+st.title("Main Page")
+st.write("Welcome to the main page of the app!")
+
